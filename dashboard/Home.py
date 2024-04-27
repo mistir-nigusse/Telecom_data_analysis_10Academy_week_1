4ee94d75efc2d66bcc88e2ed87ed3b45c714d513
mistirnigusse <mistirnigusse00@gmail.com>
2024-04-27 23:15:23 +0300

    Dashboard initial

---
+++ dashboard/Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+
+# Custom Header (Navigation)
+# st.markdown(
+#     """
+#     <div class="header">
+#         User Analytics In Telecommunication Industry <span style='color: green;'> TellCo Dashboard<span style='color: #ff6666;'>
+#     </div>
+#     """,
+#     unsafe_allow_html=True,
+# )
+
+st.title("User Analytics In Telecommunication Industry : TellCo Dashboard")
+st.markdown("""
+
+This interactive dashboard provides a comprehensive view of the key insights gleaned from our exploration of the Teleco Dataset. This Dashboard provides findings in EDA of user overview, engagmenet, satisfaction and experience.
+            Also in depth Interpretion and visualization together with further recommendation and suggestion is provided. Feel Free to navigate this dashboard using the left side navigation bar
+ """)
